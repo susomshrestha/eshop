4916f20ad2558fe9e905b5e4eb97ddd233ce9a77
susom <susomshrestha@gmail.com>
2021-11-02 11:08:42 +0545

    add user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,44 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({});
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: String,
+    default: false,
+  },
+  country: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  street: {
+    type: String,
+  },
+});
+
+userSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+userSchema.set('toJSON', {
+  virtuals: true,
+});
 
 const User = mongoose.model('User', userSchema);
 
