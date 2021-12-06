7857286a76a6138dafb6b6220c91cc6b3e29e4fa
susom <susomshrestha@gmail.com>
2021-12-06 11:37:46 +0545

    sticky header

--- frontend/src/app/modules/shop/components/header/header.component.ts
+++ frontend/src/app/modules/shop/components/header/header.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('stickyMenu') headerEle!: ElementRef;
 
-  constructor() { }
+  headerHeight: Number = 0;
 
-  ngOnInit(): void {
+  isSticky: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    this.isSticky = window.pageYOffset >= this.headerHeight;
   }
 
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.headerHeight = this.headerEle.nativeElement.offsetHeight;
+  }
+
+  ngOnInit(): void {}
 }
